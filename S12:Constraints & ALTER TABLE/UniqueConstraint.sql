73aeca3263e1b2cfd72d4fcb885376d5112a42f0
ryan <rhynogolfer42@gmail.com>
2024-05-18 14:47:11 -0700

    added section 12 folder and unique constraint

---
+++ S12:Constraints & ALTER TABLE/UniqueConstraint.sql
@@ -0,0 +1,11 @@
+CREATE TABLE contacts (
+	name VARCHAR(100) NOT NULL,
+    phone VARCHAR(15) NOT NULL UNIQUE
+);
+ 
+INSERT INTO contacts (name, phone)
+VALUES ('billybob', '8781213455');
+ 
+-- This insert would result in an error:
+INSERT INTO contacts (name, phone)
+VALUES ('billybob', '8781213455');
