b615ab82587b1d67fed93b5d1a884c0039fb35e0
ryan <rhynogolfer42@gmail.com>
2024-05-20 13:31:38 -0700

    added Left Join

---
+++ S12:One to Many & Joins/LeftJoin.sql
@@ -0,0 +1,14 @@
+SELECT 
+    first_name, last_name, order_date, amount
+FROM
+    customers
+        LEFT JOIN
+    orders ON orders.customer_id = customers.id;
+ 
+ 
+SELECT 
+    order_date, amount, first_name, last_name
+FROM
+    orders
+        LEFT JOIN
+    customers ON orders.customer_id = customers.id;
