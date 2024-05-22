d829f0602702a790d57d2b178ebcdeac6e24c704
ryan <rhynogolfer42@gmail.com>
2024-05-21 18:30:59 -0700

    final challenge done

---
+++ S14:Many to Many/TVseriesChallenge7.sql
@@ -0,0 +1,38 @@
+-- Take each review and fill in title and full name of who left it
+
+SELECT 
+    title,
+    rating,
+    CONCAT(first_name, ' ', last_name) AS reviewer
+FROM
+    reviews
+        INNER JOIN
+    series ON reviews.series_id = series.id
+        INNER JOIN
+    reviewers ON reviews.reviewer_id = reviewers.id;
+ 
+ 
+ 
+SELECT 
+    title,
+    rating,
+    CONCAT(first_name, ' ', last_name) AS reviewer
+FROM
+    series
+        INNER JOIN
+    reviews ON reviews.series_id = series.id
+        INNER JOIN
+    reviewers ON reviews.reviewer_id = reviewers.id;
+ 
+ 
+ 
+SELECT 
+    title,
+    rating,
+    CONCAT(first_name, ' ', last_name) AS reviewer
+FROM
+    reviewers
+        INNER JOIN
+    reviews ON reviews.reviewer_id = reviewers.id
+        INNER JOIN
+    series ON reviews.series_id = series.id;
