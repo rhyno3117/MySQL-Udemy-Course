2b4522ad9c96e10c4ec60c841b249d7da37c7d95
ryan <rhynogolfer42@gmail.com>
2024-05-28 12:48:37 -0700

    lead and lag code/ end of window functions section

---
+++ S16:Window Functions/Lead&Lag.sql
@@ -0,0 +1,13 @@
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    salary - LAG(salary) OVER(ORDER BY salary DESC) as salary_diff
+FROM employees;
+ 
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    salary - LAG(salary) OVER(PARTITION BY department ORDER BY salary DESC) as dept_salary_diff
+FROM employees;
