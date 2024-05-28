e93705cced15df25c262a9e13824d048228f7f14
ryan <rhynogolfer42@gmail.com>
2024-05-28 12:35:36 -0700

    added first_value code

---
+++ S16:Window Functions/First_Value.sql
@@ -0,0 +1,7 @@
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    FIRST_VALUE(emp_no) OVER(PARTITION BY department ORDER BY salary DESC) as highest_paid_dept,
+    FIRST_VALUE(emp_no) OVER(ORDER BY salary DESC) as highest_paid_overall
+FROM employees;
