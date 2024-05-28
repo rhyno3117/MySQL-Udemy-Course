c15f5099701481435b2bde8e183a77adafeb65b4
ryan <rhynogolfer42@gmail.com>
2024-05-28 11:56:19 -0700

    added partition by code

---
+++ S16:Window Functions/PartitionBy.sql
@@ -0,0 +1,22 @@
+SELECT 
+    emp_no, 
+    department, 
+    salary, 
+    AVG(salary) OVER(PARTITION BY department) AS dept_avg,
+    AVG(salary) OVER() AS company_avg
+FROM employees;
+ 
+SELECT 
+    emp_no, 
+    department, 
+    salary, 
+    COUNT(*) OVER(PARTITION BY department) as dept_count
+FROM employees;
+ 
+SELECT 
+    emp_no, 
+    department, 
+    salary, 
+    SUM(salary) OVER(PARTITION BY department) AS dept_payroll,
+    SUM(salary) OVER() AS total_payroll
+FROM employees;
