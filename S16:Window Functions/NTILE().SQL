e56e9ffc81a3d5b17d9659be9b16ed519fab979b
ryan <rhynogolfer42@gmail.com>
2024-05-28 12:30:16 -0700

    added Ntile() code

---
+++ S16:Window Functions/NTILE().SQL
@@ -0,0 +1,7 @@
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    NTILE(4) OVER(PARTITION BY department ORDER BY salary DESC) AS dept_salary_quartile,
+	NTILE(4) OVER(ORDER BY salary DESC) AS salary_quartile
+FROM employees;
