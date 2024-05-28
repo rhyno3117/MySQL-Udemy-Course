5999dad33605d288dd025e236774d4622296eeb7
ryan <rhynogolfer42@gmail.com>
2024-05-28 12:04:45 -0700

    added order by with windows code

---
+++ S16:Window Functions/OrderByWithWindows.sql
@@ -0,0 +1,14 @@
+SELECT 
+    emp_no, 
+    department, 
+    salary, 
+    SUM(salary) OVER(PARTITION BY department ORDER BY salary) AS rolling_dept_salary,
+    SUM(salary) OVER(PARTITION BY department) AS total_dept_salary
+FROM employees;
+ 
+SELECT 
+    emp_no, 
+    department, 
+    salary, 
+    MIN(salary) OVER(PARTITION BY department ORDER BY salary DESC) as rolling_min
+FROM employees;
