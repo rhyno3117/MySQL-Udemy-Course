5494e6bb6d112b7593f4eea2169d2f508ea9c09c
ryan <rhynogolfer42@gmail.com>
2024-05-30 12:25:48 -0700

    added instagram first step of code

---
+++ Instagram/ig_clone.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE ig_clone;
+USE ig_clone;
+
+CREATE TABLE users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+DESCRIBE users;
+ 
