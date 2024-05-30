bd451a2db288b40043a331ff6b2b2eeed24f7cb3
ryan <rhynogolfer42@gmail.com>
2024-05-30 12:37:18 -0700

    created table for photos

--- Instagram/ig_clone.sql
+++ Instagram/ig_clone.sql
@@ -7,11 +7,11 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT NOW()
 );
 
-DESCRIBE users;
- 
-INSERT INTO users (username) VALUES
-('BlueTheCat'),
-('CharlieBrown'),
-('ColtSteele');
+CREATE TABLE photos (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
 
-SELECT * FROM users;
