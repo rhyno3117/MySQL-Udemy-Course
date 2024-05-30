bb53361bc20ac2c8a1fde7e39ae7bc6bf5fd78db
ryan <rhynogolfer42@gmail.com>
2024-05-30 13:36:59 -0700

    added tags and photo_tags table

--- Instagram/ig_clone.sql
+++ Instagram/ig_clone.sql
@@ -42,3 +42,16 @@ CREATE TABLE follows(
     FOREIGN KEY(followee_id) REFERENCES users(id),
     PRIMARY KEY(follower_id, followee_id)
 );
+
+CREATE TABLE tags(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+CREATE TABLE photo_tags(
+    photo_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id, tag_id)
+);
