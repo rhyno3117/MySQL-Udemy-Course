59cbdff1f88085df535d7f852adce527fa8e244c
ryan <rhynogolfer42@gmail.com>
2024-05-30 12:44:55 -0700

    added table comments

--- Instagram/ig_clone.sql
+++ Instagram/ig_clone.sql
@@ -15,3 +15,12 @@ CREATE TABLE photos (
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
+CREATE TABLE comments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id)
+);
