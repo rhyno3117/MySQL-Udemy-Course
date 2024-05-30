5e82dc3c1ca2303105a409d3416d263e350e014e
ryan <rhynogolfer42@gmail.com>
2024-05-30 12:57:30 -0700

    added likes table

--- Instagram/ig_clone.sql
+++ Instagram/ig_clone.sql
@@ -24,3 +24,12 @@ CREATE TABLE comments (
     FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(photo_id) REFERENCES photos(id)
 );
+
+CREATE TABLE likes(
+user_id INTEGER NOT NULL,
+photo_id INTEGER NOT NULL,
+created_at TIMESTAMP DEFAULT NOW(),
+FOREIGN KEY(user_id) REFERENCES users(id),
+FOREIGN KEY(photo_id) REFERENCES photos(id),
+PRIMARY KEY(user_id, photo_id)
+);
