d285e0297bf9ff358bc06d0bffb281bdc8e6ee4d
ryan <rhynogolfer42@gmail.com>
2024-05-30 13:04:56 -0700

    added follows table

--- Instagram/ig_clone.sql
+++ Instagram/ig_clone.sql
@@ -26,10 +26,19 @@ CREATE TABLE comments (
 );
 
 CREATE TABLE likes(
-user_id INTEGER NOT NULL,
-photo_id INTEGER NOT NULL,
-created_at TIMESTAMP DEFAULT NOW(),
-FOREIGN KEY(user_id) REFERENCES users(id),
-FOREIGN KEY(photo_id) REFERENCES photos(id),
-PRIMARY KEY(user_id, photo_id)
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    PRIMARY KEY(user_id, photo_id)
+);
+
+CREATE TABLE follows(
+    follower_id INTEGER NOT NULL,
+    followee_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
 );
