dccb604a72aab0bc918e607dad33c0db155bb5fb
ryan <rhynogolfer42@gmail.com>
2024-05-31 12:47:51 -0700

    solution 2

--- Instagram/ig_cloneQuestions.sql
+++ Instagram/ig_cloneQuestions.sql
@@ -3,3 +3,14 @@
 SELECT * FROM users
 ORDER BY created_at
 LIMIT 5;
+
+-- What day of the week do most users register on?
+-- We need to figure out when to schedule and ad campaign.
+-- Most popular registration date
+SELECT 
+    DAYNAME(created_at) AS day,
+    COUNT(*) AS total
+FROM users
+GROUP BY day
+ORDER BY total DESC
+LIMIT 2;
