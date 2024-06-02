b6c92ca3c7f8204a3e4d6d27c80b2945bd6cabb0
ryan <rhynogolfer42@gmail.com>
2024-06-02 15:53:53 -0700

    created function to generate address

--- Project.1/app.js
+++ Project.1/app.js
@@ -1,5 +1,13 @@
 var { faker } = require('@faker-js/faker');
 
-console.log(faker.internet.email());
+// console.log(faker.internet.email());
+// console.log(faker.date.past());
 
-console.log(faker.date.past());
+function generateAddress(){
+    console.log(faker.location.streetAddress());
+    console.log(faker.location.city());
+    console.log(faker.location.state());
+  }
+
+  generateAddress(); 
+
