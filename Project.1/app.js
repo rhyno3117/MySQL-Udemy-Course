27a6fe0af20aaeac2db72e1fa3365a07646a01c2
ryan <rhynogolfer42@gmail.com>
2024-06-03 08:42:43 -0700

    installed mysql

--- Project.1/app.js
+++ Project.1/app.js
@@ -3,11 +3,11 @@ var { faker } = require('@faker-js/faker');
 // console.log(faker.internet.email());
 // console.log(faker.date.past());
 
-function generateAddress(){
-    console.log(faker.location.streetAddress());
-    console.log(faker.location.city());
-    console.log(faker.location.state());
-  }
+// function generateAddress(){
+//     console.log(faker.location.streetAddress());
+//     console.log(faker.location.city());
+//     console.log(faker.location.state());
+//   }
 
-  generateAddress(); 
+//   generateAddress(); 
 
