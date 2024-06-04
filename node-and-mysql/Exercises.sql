f24aec05ecec00cef1b434fb5e0079653e31ca52
ryan <rhynogolfer42@gmail.com>
2024-06-04 10:49:30 -0700

    updated

--- node-and-mysql/Exercises.sql
+++ node-and-mysql/Exercises.sql
@@ -1,15 +1,18 @@
 -- Challenge 1
+-- Find Earliest Date A User Joined
 SELECT 
     DATE_FORMAT(MIN(created_at), "%M %D %Y") as earliest_date 
 FROM users;
 
 -- Challenge 2
+-- Find Email of Earliest User
 SELECT * 
 FROM   users 
 WHERE  created_at = (SELECT Min(created_at) 
                      FROM   users); 
 
 -- Challenge 3
+-- Users According to the Month they Joined
 SELECT Monthname(created_at) AS month, 
        Count(*)              AS count 
 FROM   users 
@@ -17,12 +20,13 @@ GROUP  BY month
 ORDER  BY count DESC; 
 
 -- Challenge 4
-
+-- Cound Number of Users with Yahoo Emails
 SELECT Count(*) AS yahoo_users 
 FROM   users 
 WHERE  email LIKE '%@yahoo.com'; 
 
 -- Challenge 5
+-- Calculate Total Number of Users for Each Email Host
 SELECT CASE 
          WHEN email LIKE '%@gmail.com' THEN 'gmail' 
          WHEN email LIKE '%@yahoo.com' THEN 'yahoo' 
