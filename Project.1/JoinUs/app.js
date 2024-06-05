1abc9ac8dfd5b181975910fa83e3046fc3a1b508
ryan <rhynogolfer42@gmail.com>
2024-06-05 12:04:42 -0700

    code shows send message on web page on http://localhost:8080/

--- Project.1/JoinUs/app.js
+++ Project.1/JoinUs/app.js
@@ -1,2 +1,10 @@
 var express = require('express');
 var app = express();
+
+app.get("/", function(req, res){
+    res.send("You've reached the home page")
+});
+
+app.listen(8080, function(){
+    console.log("Server running on port 8080!");
+});
