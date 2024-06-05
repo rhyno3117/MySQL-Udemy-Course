643322c668832d06d00bec3c6776862526b258cb
ryan <rhynogolfer42@gmail.com>
2024-06-05 12:43:59 -0700

    added connection to mysql in app.js

--- Project.1/JoinUs/app.js
+++ Project.1/JoinUs/app.js
@@ -3,7 +3,15 @@
 // Started up server to listen and make it run
 // Request is sent on server http://localhost:8080 and we see message with / addded
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'cocomysql',
+    database: 'join_us'
+});
 
 app.get("/", function (req, res) {
     res.send("You've reached the home page")
