584a20cb972e2618bc151e89697c136cd144762f
ryan <rhynogolfer42@gmail.com>
2024-06-05 17:29:32 -0700

    css started

--- Project.1/JoinUs/app.js
+++ Project.1/JoinUs/app.js
@@ -9,6 +9,7 @@ var app = express();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
 var connection = mysql.createConnection({
     host: 'localhost',
